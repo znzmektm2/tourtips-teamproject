442ba45d60701d9c9baebce5a12089544e54b0dc
hkarling <dantert@gmail.com>
2019-07-10 11:51:36 +0900

    fix: recover files, commentSelectByPlace

--- src/project/controller/comment/CommentSelectByPlaceServlet.java
+++ src/project/controller/comment/CommentSelectByPlaceServlet.java
@@ -18,22 +18,25 @@ import project.model.service.CommentService;
 import project.model.service.CommentServiceImpl;
 
 @SuppressWarnings("serial")
-@WebServlet("/CommentSelect")
+@WebServlet("/CommentSelectByPlace")
 public class CommentSelectByPlaceServlet extends HttpServlet {
-	
+
 	private CommentService service = CommentServiceImpl.getInstance();
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PlaceDTO place = (PlaceDTO) request.getAttribute("location");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("CommentSelectByPlaceServletCalled");
+		String placeId = request.getParameter("placeId");
 		List<CommentDTO> list = null;
-		
+
 		try {
-			list = service.selectByPlaceId(place.getId());
+			list = service.selectByPlaceId(Integer.parseInt(placeId));
 
 			JSONArray jsonArr = JSONArray.fromObject(list);
-	        response.setContentType("text/html;charset=UTF-8");
-	            PrintWriter out = response.getWriter();
-	        out.println(jsonArr);
+			System.out.println(jsonArr);
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println(jsonArr);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
